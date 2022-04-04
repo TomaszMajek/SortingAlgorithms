15e993047170f86033f880b7c101db4452871d6d
Tomasz Majek <majek.tomek@gmail.com>
2022-04-04 17:18:11 +0200

    Bucket sort update

--- SortingAlgorithms/BucketSort.cs
+++ SortingAlgorithms/BucketSort.cs
@@ -8,5 +8,38 @@ namespace SortingAlgorithms.SortingAlgorithms
 {
     internal class BucketSort
     {
+        // we need to divide whole array[] into smaller chunks (buckets)
+        // unsorted array (0/99) into
+        // 0...9   | 0 bucket
+        // 10...19 | 1 bucket
+        // .......
+        // 90...99 | 9 bucket
+
+        private int n;
+        private int m;
+        private int[] array;
+        public int[] solution;
+
+        public BucketSort(int[] array)
+        {
+            n = array.Length;
+            this.array = array;
+            solution = Sorted();
+        }
+
+        private int[] Sorted()
+        {
+            List<int> bucketsList = new List<int>();
+
+            for (int i = 0; i < m; i++)
+            {
+                n = m * array[i];
+                bucketsList[n] = array[i];
+            }
+
+            return array;
+        }
+
+
     }
 }
